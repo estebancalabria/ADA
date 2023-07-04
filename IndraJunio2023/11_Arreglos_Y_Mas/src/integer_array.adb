c2b0997497f56df270c447ea1ee186486513650d
Esteban Calabria <esteban.calabria@gmail.com>
2023-07-04 10:00:04 -0300

    Faltaba

--- IndraJunio2023/11_Arreglos_Y_Mas/src/integer_array.adb
+++ IndraJunio2023/11_Arreglos_Y_Mas/src/integer_array.adb
@@ -1,5 +1,92 @@
-package body Integer_Array is
+with Ada.Text_IO; use Ada.Text_IO;
 
+package body Integer_Array is
+  
+
+   procedure Mostrar_Numeros(Lista_Numeros :in Conjunto_De_Numeros) is
+   begin
+      Put("(");
+      for I in Lista_Numeros'Range loop
+         Put(Lista_Numeros(I)'Image);
+         
+         if (I /= Lista_Numeros'Last) then
+            Put(",");
+         end if;
+      end loop;   
+      Put(")");
+      New_Line;
+   end Mostrar_Numeros;
+   
+   function Crear(Cantidad:Integer; Default : Integer) return Conjunto_De_Numeros is
+        Aux : Conjunto_De_Numeros(1..Cantidad) := (others => Default);
+   begin
+        return Aux;
+   end Crear;
+   
+   function Crear_Secuencia(Valor_Inicial:Integer; Valor_Final: Integer) return Conjunto_De_Numeros is
+      N : Conjunto_De_Numeros (Valor_Inicial .. Valor_Final);   
+   begin
+      for I in N'Range loop
+         N(I) := I;      
+      end loop;            
+      return N;
+   end Crear_Secuencia;
+   
+    procedure Revertir(Lista_Numeros: in out Conjunto_De_Numeros) is
+      Aux : Integer;   
+      Ultimo_Actual : Integer := Lista_Numeros'Last;
+   begin      
+      for I in Lista_Numeros'First..Lista_Numeros'Last/2 loop
+         AUX := Lista_Numeros(I);
+         Lista_Numeros(I) := Lista_Numeros(Ultimo_Actual);
+         Lista_Numeros(Ultimo_Actual) := AUX;      
+         Ultimo_Actual := Ultimo_Actual -1;
+      end loop;   
+   end Revertir;
+   
+   function "&" (Source:Conjunto_De_Numeros; Dest:Conjunto_De_Numeros) return Conjunto_De_Numeros is
+   begin
+      --SE animan a hacer el concatwnar?
+     return Source;
+   end;
+   --  
+   --  function "&" (Source:Numeros; Dest:Integer) return Numeros
+   --  begin
+   --     return Source & Dest;
+   --  end;
+   
+   function Slice(Source : Conjunto_De_Numeros; From : Integer; To : Integer) return Conjunto_De_Numeros is
+   begin
+      return Source(From..To);   
+   end Slice;
+   
+   function Pertenece (Valor : Integer; Arreglo : Conjunto_De_Numeros) return Boolean is
+      Ok : Boolean := False;      
+      I : Integer := Arreglo'First;   
+   begin      
+      while (not Ok) and (I <= Arreglo'Last) loop
+         if Arreglo(I) = Valor then
+            Ok := True;         
+         end if;         
+         I := I+1;      
+      end loop;            
+      return Ok;   
+   end Pertenece;
+   
+   function Agregar (Valor : in Integer; Arreglo : in Conjunto_De_Numeros) return Conjunto_De_Numeros is
+   begin
+      return (if (Pertenece(Valor,Arreglo)) then Arreglo else Arreglo & Valor);   
+      
+      --  if (Pertenece(Valor,Arreglo)) then
+      --     return Arreglo;
+      --  end if;
+      --  return Arreglo & Valor;
+   end Agregar;
+   
+   --  procedure Add(Valor:integer; Arreglo:in out Numeros) is
+   --  begin
+   --     Arreglo := Arreglo & Valor;
+   --  end;
    
 
 end Integer_Array;
--- IndraJunio2023/11_Arreglos_Y_Mas/src/main.adb
+++ IndraJunio2023/11_Arreglos_Y_Mas/src/main.adb
@@ -1,6 +1,53 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Integer_Array; use Integer_Array;
+
 procedure Main is
 
 begin
-   --  Insert code here.
-   null;
+   declare
+      --N : Numeros; --Me tira un error porque no sabe cuanta memoria necesita
+      N : Conjunto_De_Numeros:= Crear(5,Default => 10);  --Creo implicitamente ej: s:string:= "12"
+   begin
+      --N := (1,2,3,4,5); --Esta linea no funciona porque el tipo numeros es privado
+      N := Crear_Secuencia(1,5);
+      Mostrar_Numeros(N);
+
+      declare
+         N_Mas_6 : Conjunto_De_Numeros := Agregar(6, N); --IMPORTANTE : Si o si tengo que declarar otra variable
+         N_Mas_Otro_6 : Conjunto_De_Numeros := Agregar (6, N_Mas_6);
+      begin
+         Mostrar_Numeros(N_Mas_6);
+         Mostrar_Numeros(N_Mas_Otro_6);
+      end;
+
+      if Pertenece(5,N) then
+         Put_Line("El 5 esta en la secuencia");
+      end if;
+
+      --  if 5 in N then
+      --     Put_Line("dsd");
+      --  end if;
+
+      Mostrar_Numeros(N);
+      Revertir(N);
+      Mostrar_Numeros(N);
+
+      declare
+         Na : Conjunto_De_Numeros := Crear_Secuencia(1,3);
+         Nb : Conjunto_De_Numeros := Crear_Secuencia(3,6);
+         Res : Conjunto_De_Numeros := Na & Nb;
+         --N4 : Numeros := N3 & 4;
+        -- N5 : Numeros := Slice(N3,3,5); --Subarreglos, tiene que estar dentro del rango
+         --N6 :Numeros (1..2) := (1=>1, 2=><>);
+      begin
+         Mostrar_Numeros(Res);
+         --Mostrar_Numeros(N4);
+         --Mostrar_Numeros(N5);
+         --Mostrar_Numeros(N6);
+         null;
+      end;
+
+   end;
+
+
 end Main;
